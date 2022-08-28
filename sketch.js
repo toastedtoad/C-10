8531b974801e0956b8b7e835533479b8b81d80ee
toastedtoad <110530204+toastedtoad@users.noreply.github.com>
2022-08-28 10:17:36 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var aaaa = [2,4,6,8]
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  sum =aaaa[0]+ aaaa[1]+ aaaa[2]+ aaaa[3]
+  average = sum/4
+  console.log (average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
